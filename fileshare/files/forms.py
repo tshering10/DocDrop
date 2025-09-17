3fa5547adefaca76a63357d33591992c4392b1e6
Tshering00 <tshering.work1@gmail.com>
2025-09-17 16:25:28 +0545

    Style file upload page

--- fileshare/files/forms.py
+++ fileshare/files/forms.py
@@ -6,8 +6,19 @@ class FileUploadForm(forms.ModelForm):
         model = File
         fields = ['title', 'file', 'category']
         
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'px-3 py-2 mt-1 mb-3 border border-gray-800 rounded-lg bg-gray-800 text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': "Name the file.."
+            }),
+            'file': forms.ClearableFileInput(attrs={
+                'class': 'px-3 py-2 mt-1 mb-3 border border-gray-800 rounded-lg bg-gray-800 pointer  text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            })
+        }
+        
+        
     def clean(self):
-        file = self.clean_data.get('file')
+        file = self.cleaned_data.get('file')
         
         #file size validation
         if file.size > 20 * 1024 * 1024:
