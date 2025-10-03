a01ae49f429cebf7103e2a2d36c7d8914af909f1
Tshering00 <tshering.work1@gmail.com>
2025-10-03 16:11:48 +0545

    feat(auth): Add password toggle button to login page

--- fileshare/accounts/forms.py
+++ fileshare/accounts/forms.py
@@ -22,14 +22,6 @@ class CustomUserCreationForm(UserCreationForm):
             raise forms.ValidationError("Username must be at least 3 characters long.")
         return username
     
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     password1 = cleaned_data.get('password1')
-    #     password2 = cleaned_data.get('password2')
-        
-    #     if password1 and password2 and password1 != password2:
-    #         raise forms.ValidationError("Passwords did not match.")
-    #     return cleaned_data
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
